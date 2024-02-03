8c07650994f5a8f2e4fdf483144287a161dee855
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-02-03 10:58:37 +0300

    Extract Header

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,29 +1,14 @@
 <script setup>
-import { Head } from "@inertiajs/vue3";
 import Banner from "@/Components/Banner.vue";
 import PrimaryMenu from "@/Navs/PrimaryMenu.vue";
-
-defineProps({
-  title: String,
-});
 </script>
 
 <template>
   <div>
-    <Head :title="title" />
-
     <Banner />
     <PrimaryMenu />
 
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-      <!-- Page Heading -->
-      <header v-if="$slots.header" class="bg-white dark:bg-gray-800 shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <slot name="header" />
-        </div>
-      </header>
-
-      <!-- Page Content -->
       <main>
         <slot />
       </main>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,23 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import { Head } from "@inertiajs/vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Header from "@/Layouts/Header.vue";
+import Welcome from "@/Components/Welcome.vue";
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+  <Head title="Dashboard" />
+  <AppLayout>
+    <Header title="Dashboard" />
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div
+          class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
+        >
+          <Welcome />
         </div>
-    </AppLayout>
+      </div>
+    </div>
+  </AppLayout>
 </template>
