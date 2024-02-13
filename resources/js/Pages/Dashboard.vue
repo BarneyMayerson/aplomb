c0985ec95b3838d9b430e62d13a858d8138a57fe
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-02-13 15:38:13 +0300

    Examine  jetstream dialog modal

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,11 @@
 <script setup>
+import { ref } from "vue";
 import Header from "@/Layouts/Header.vue";
 import Welcome from "@/Components/Welcome.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import DialogModal from "@/Components/DialogModal.vue";
+
+const showDialogModal = ref(false);
 </script>
 
 <template>
@@ -11,8 +16,50 @@ import Welcome from "@/Components/Welcome.vue";
       <div
         class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
       >
+        <div class="my-4 sm:px-6 lg:px-8">
+          <div class="flex space-x-3">
+            <SecondaryButton @click="showDialogModal = true">
+              Dialog Modal
+            </SecondaryButton>
+          </div>
+        </div>
         <Welcome />
       </div>
     </div>
   </div>
+
+  <DialogModal
+    :show="showDialogModal"
+    @close="showDialogModal = false"
+    max-width="lg"
+  >
+    <template #title> Header goes here </template>
+    <template #content>
+      <div class="overflow-y-auto max-h-96 p-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis,
+        earum vitae inventore voluptate facere tempora minima consequatur
+        aperiam mollitia odio, neque delectus praesentium suscipit nulla magnam
+        perferendis ullam corrupti! Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Eos, autem! Ab ad nesciunt voluptatum pariatur
+        quisquam temporibus debitis expedita dignissimos possimus officia? Harum
+        accusamus atque soluta dolor a accusantium hic! Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Laudantium perspiciatis recusandae
+        porro quae, aut perferendis maiores expedita et iste fugiat dolore
+        voluptates fuga ex odio, asperiores a dolor illo ipsa. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Magni nobis, earum vitae
+        inventore voluptate facere tempora minima consequatur aperiam mollitia
+        odio, neque delectus praesentium suscipit nulla magnam perferendis ullam
+        corrupti! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos,
+        autem! Ab ad nesciunt voluptatum pariatur quisquam temporibus debitis
+        expedita dignissimos possimus officia? Harum accusamus atque soluta
+        dolor a accusantium hic! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium perspiciatis recusandae porro quae, aut
+        perferendis maiores expedita et iste fugiat dolore voluptates fuga ex
+        odio, asperiores a dolor illo ipsa.
+      </div>
+    </template>
+    <template #footer>
+      <div class="text-sky-500">Footer goes here</div>
+    </template>
+  </DialogModal>
 </template>
