80605d68bf78ce00890d25bc15396199f564fa37
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-02-26 11:39:49 +0300

    Life hack for TextInput  smooth appearance when onMounted() hook is occurred [VI]

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,28 +1,30 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 
 defineProps({
-    modelValue: String,
+  modelValue: String,
 });
 
-defineEmits(['update:modelValue']);
+defineEmits(["update:modelValue"]);
 
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
+  setTimeout(() => {
+    if (input.value.hasAttribute("autofocus")) {
+      input.value.focus();
     }
+  }, 1);
 });
 
 defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-    <input
-        ref="input"
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-    >
+  <input
+    ref="input"
+    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
 </template>
