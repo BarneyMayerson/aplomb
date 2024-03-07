80266661f0478f6e48b4c883be2a3eb62453abb3
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-03-07 15:21:55 +0300

    Clean Welcome page (wip)

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,32 +13,29 @@ defineProps({
 <template>
   <Head title="Welcome" />
 
-  <div
-    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-gray-100 dark:bg-gray-900"
+  <nav
+    class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700"
   >
-    <div class="sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10 flex items-center">
-      <ThemeDropdown class="px-2" />
-      <div v-if="!$page.props.auth.user">
-        <Link
-          v-if="canLogin"
-          :href="route('login')"
-          class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-        >
-          Log in
-        </Link>
+    <div
+      v-if="!$page.props.auth.user"
+      class="py-3 max-w-7xl flex mx-auto items-center justify-end"
+    >
+      <ThemeDropdown class="inline-flex" />
+      <Link
+        v-if="canLogin"
+        :href="route('login')"
+        class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+      >
+        Log in
+      </Link>
 
-        <Link
-          v-if="canRegister"
-          :href="route('register')"
-          class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-        >
-          Register
-        </Link>
-      </div>
+      <Link
+        v-if="canRegister"
+        :href="route('register')"
+        class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+      >
+        Register
+      </Link>
     </div>
-
-    <div class="max-w-7xl mx-auto p-6 lg:p-8 border-green-500">
-      Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
-    </div>
-  </div>
+  </nav>
 </template>
