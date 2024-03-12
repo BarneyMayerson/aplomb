7bc4ffa9109212dba365a9a616f2a3b6861bae96
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-03-12 10:49:22 +0300

    Adding gradient background to a relative element

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -28,7 +28,6 @@ defineProps({
       >
         Log in
       </Link>
-
       <Link
         v-if="canRegister"
         :href="route('register')"
@@ -38,11 +37,28 @@ defineProps({
       </Link>
     </div>
   </nav>
+
   <section class="border border-yellow-400">
     <div
       class="border border-fuchsia-400 relative container flex flex-col mx-auto"
     >
-      <div class="lg:flex-center">
+      <div class="m-1 border border-green-400 z-0">
+        <video autoplay muted loop class="w-full h-full grayscale">
+          <source src="/storage/nfsu-trailer.mp4" type="video/mp4" />
+          Your browser does not support HTML5 video.
+        </video>
+        <div
+          class="absolute inset-0"
+          style="
+            background-image: radial-gradient(
+              circle at 2% 22%,
+              rgba(2, 4, 7, 0),
+              rgb(2, 4, 7) 79%
+            );
+          "
+        ></div>
+      </div>
+      <div class="lg:flex-center mt-14 border border-purple-400 z-20">
         <h1
           class="z-20text-balance text-center font-kanit text-[50px] uppercase md:text-[7vw] lg:mb-0 lg:mt-0 lg:text-left xl:text-[8vw] 2xl:text-[9vw]"
           style="line-height: 0.8em"
