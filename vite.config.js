c25824fb6aac7affdcf09d020c988be6dacb70e4
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-03-14 16:09:46 +0300

    Vitest: install and config (include, exclude example)

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,32 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: "resources/js/app.js",
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["tests/Javascript/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+    // exclude: [
+    //   ...configDefaults.exclude,
+    //   "**/.vscode/**",
+    //   "**/{app,bootstrap,config,database,public,resources,routes,storage}/**",
+    //   "**/tests/{Feature,Unit}/**",
+    // ],
+  },
 });
