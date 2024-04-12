8e5d9e6482c75822a64925faa4f58ae75501f304
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-04-12 14:45:57 +0300

    Introducing PostCard (cabinet posts index)

---
+++ resources/js/Components/Cabinet/Posts/PostCard.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import {
+  EyeIcon,
+  PencilIcon,
+  TrashIcon,
+  BookOpenIcon,
+} from "@heroicons/vue/24/outline";
+
+defineProps({
+  post: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="flex flex-col justify-between rounded-lg px-6 py-4 bg-white dark:bg-gray-800 shadow dark:shadow-sky-400"
+  >
+    <div>
+      <p class="font-bold">{{ post.title }}</p>
+    </div>
+    <div class="mt-2 flex space-x-2 justify-end">
+      <Link
+        href="#"
+        class="p-2 rounded-md dark:text-gray-200 bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition ease-in-out duration-150"
+        title="Preview"
+      >
+        <EyeIcon class="w-5 h-5" />
+      </Link>
+      <Link
+        href="#"
+        class="p-2 rounded-md dark:text-gray-200 bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition ease-in-out duration-150"
+        title="Edit"
+      >
+        <PencilIcon class="w-5 h-5" />
+      </Link>
+      <Link
+        v-if="!post.published_at"
+        href="#"
+        class="p-2 rounded-md text-red-700 dark:text-red-200 bg-red-100 dark:bg-red-700 hover:bg-red-200 dark:hover:bg-red-600 transition ease-in-out duration-150"
+        title="Delete"
+      >
+        <TrashIcon class="w-5 h-5" />
+      </Link>
+      <Link
+        v-if="!post.published_at"
+        href="#"
+        class="p-2 rounded-md text-yellow-700 dark:text-yellow-200 bg-yellow-100 dark:bg-yellow-700 hover:bg-yellow-200 dark:hover:bg-yellow-600 transition ease-in-out duration-150"
+        title="Publish"
+      >
+        <BookOpenIcon class="w-5 h-5" />
+      </Link>
+    </div>
+  </div>
+</template>
--- resources/js/Pages/User/Cabinet/Posts/Index.vue
+++ resources/js/Pages/User/Cabinet/Posts/Index.vue
@@ -3,8 +3,8 @@ import { Head, Link } from "@inertiajs/vue3";
 import { computed } from "vue";
 import CabinetNavbar from "@/Navs/CabinetNavbar.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
-import DangerButton from "@/Components/DangerButton.vue";
 import Pagination from "@/Components/Pagination.vue";
+import PostCard from "@/Components/Cabinet/Posts/PostCard.vue";
 
 const props = defineProps(["posts"]);
 
@@ -18,40 +18,25 @@ const showPagination = computed(() => props.posts.meta.last_page > 1);
     <div class="md:grid md:grid-cols-[180px_auto]">
       <CabinetNavbar class="py-2 md:py-6" />
       <div class="px-4 py-8">
+        <div class="flex items-center justify-between">
+          <h3 class="text-xl font-semibold">Your posts.</h3>
+          <Link href="#">
+            <PrimaryButton>Create</PrimaryButton>
+          </Link>
+        </div>
         <div v-if="hasPosts">
-          <div class="flex items-center justify-between">
-            <h3 class="text-xl font-semibold">Your posts.</h3>
-            <Link href="#">
-              <PrimaryButton>Create</PrimaryButton>
-            </Link>
-          </div>
-
           <!-- Posts -->
           <div
             class="mt-8 grid gap-4 grid-cols-1 lg:grid-cols-2 lg:gap-6 xl:grid-cols-3 xl-gap-8"
           >
-            <div
-              v-for="post in posts.data"
-              :key="post.id"
-              class="rounded-lg px-6 py-3 flex flex-col justify-between bg-white dark:bg-gray-800 shadow dark:shadow-sky-400"
-            >
-              <div>
-                <p class="font-bold">{{ post.title }}</p>
-              </div>
-              <div class="flex justify-end space-x-2 w-full">
-                <PrimaryButton v-if="!post.published_at">
-                  Publish
-                </PrimaryButton>
-                <DangerButton v-if="!post.published_at">Delete</DangerButton>
-                <PrimaryButton>Preview</PrimaryButton>
-                <PrimaryButton>Edit</PrimaryButton>
-              </div>
-            </div>
+            <template v-for="post in posts.data" :key="post.id">
+              <PostCard :post="post" />
+            </template>
           </div>
 
           <Pagination v-if="showPagination" class="mt-4" :meta="posts.meta" />
         </div>
-        <div v-else>
+        <div v-else class="mt-8">
           You don't have any posts yet. You can start creating a new one right
           now. Then you can publish it whenever you want.
         </div>
