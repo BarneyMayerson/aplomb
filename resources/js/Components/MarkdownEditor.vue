24f3cd18422b8bd2af3b715f8f6d01da7d940f68
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-04-18 15:21:03 +0300

    Markdown editor toolbar with remoxicons (wip)

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -3,6 +3,7 @@ import { watch } from "vue";
 import { EditorContent, useEditor } from "@tiptap/vue-3";
 import StarterKit from "@tiptap/starter-kit";
 import { Markdown } from "tiptap-markdown";
+import "remixicon/fonts/remixicon.css";
 
 const editor = useEditor({
   extensions: [StarterKit, Markdown],
@@ -35,5 +36,153 @@ watch(
 </script>
 
 <template>
-  <EditorContent :editor />
+  <div
+    v-if="editor"
+    class="bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-white rounded-md shadow-sm border-0 ring-1 ring-inset ring-gray-300 dark:ring-gray-700"
+  >
+    <menu class="flex divide-x divide-gray-300 dark:divide-gray-700">
+      <li>
+        <button
+          @click="() => editor.chain().focus().toggleBold().run()"
+          type="button"
+          class="px-3 py-2 rounded-tl-md"
+          :class="[
+            editor.isActive('bold')
+              ? 'bg-indigo-300 dark:bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Bold"
+        >
+          <i class="ri-bold"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="() => editor.chain().focus().toggleItalic().run()"
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('italic')
+              ? 'bg-indigo-300 dark:bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Italic"
+        >
+          <i class="ri-italic"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="() => editor.chain().focus().toggleStrike().run()"
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('strike')
+              ? 'bg-indigo-300 dark:bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Strikethrough"
+        >
+          <i class="ri-strikethrough"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="() => editor.chain().focus().toggleBlockquote().run()"
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('blockquote')
+              ? 'bg-indigo-300 dark:bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Blockquote"
+        >
+          <i class="ri-double-quotes-l"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="() => editor.chain().focus().toggleBulletList().run()"
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('bulletList')
+              ? 'bg-indigo-300 dark:bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Bullet list"
+        >
+          <i class="ri-list-unordered"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="() => editor.chain().focus().toggleOrderedList().run()"
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('orderedList')
+              ? 'bg-indigo-300 dark:bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Numeric list"
+        >
+          <i class="ri-list-ordered"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="
+            () => editor.chain().focus().toggleHeading({ level: 2 }).run()
+          "
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('heading', { level: 2 })
+              ? 'bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Heading 1"
+        >
+          <i class="ri-h-1"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="
+            () => editor.chain().focus().toggleHeading({ level: 3 }).run()
+          "
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('heading', { level: 3 })
+              ? 'bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Heading 2"
+        >
+          <i class="ri-h-2"></i>
+        </button>
+      </li>
+      <li>
+        <button
+          @click="
+            () => editor.chain().focus().toggleHeading({ level: 4 }).run()
+          "
+          type="button"
+          class="px-3 py-2"
+          :class="[
+            editor.isActive('heading', { level: 4 })
+              ? 'bg-indigo-500'
+              : 'hover:bg-gray-200 dark:hover:bg-gray-600',
+          ]"
+          title="Heading 3"
+        >
+          <i class="ri-h-3"></i>
+        </button>
+      </li>
+    </menu>
+    <EditorContent :editor />
+  </div>
 </template>
