c9280054a9b41ea62304c66e2c4f35b4e8cd6fbe
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-04-18 11:02:18 +0300

    Two ways model binding for the MarkdownEditor

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -1,15 +1,35 @@
 <script setup>
+import { watch } from "vue";
 import { EditorContent, useEditor } from "@tiptap/vue-3";
 import StarterKit from "@tiptap/starter-kit";
 
 const editor = useEditor({
   extensions: [StarterKit],
+  onUpdate: () => emit("update:modelValue", editor.value?.getHTML()),
   editorProps: {
     attributes: {
-      class: `bg-white dark:bg-gray-900 ring-1 ring-inset ring-gray-300 dark:ring-gray-700 prose dark:prose-invert max-w-none rounded-b-md focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:focus:ring-indigo-600 py-1.5 px-3 min-h-[512px]`,
+      class: `bg-white dark:bg-gray-900 ring-1 ring-inset ring-gray-300 dark:ring-gray-700 prose dark:prose-invert max-w-none rounded-b-md focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:focus:ring-indigo-600 py-1.5 px-3 min-h-[100px]`,
     },
   },
 });
+
+const props = defineProps({
+  modelValue: "",
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    if (value === editor.value?.getHTML()) {
+      return;
+    }
+
+    editor.value?.commands.setContent(value);
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
