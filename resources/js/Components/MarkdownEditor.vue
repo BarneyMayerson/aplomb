c489cf9e39d670ad344380d5ebf86b1f19ea7086
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-04-18 10:20:50 +0300

    Styling the editor

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -4,6 +4,11 @@ import StarterKit from "@tiptap/starter-kit";
 
 const editor = useEditor({
   extensions: [StarterKit],
+  editorProps: {
+    attributes: {
+      class: `bg-white dark:bg-gray-900 ring-1 ring-inset ring-gray-300 dark:ring-gray-700 prose dark:prose-invert max-w-none rounded-b-md focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:focus:ring-indigo-600 py-1.5 px-3 min-h-[512px]`,
+    },
+  },
 });
 </script>
 
