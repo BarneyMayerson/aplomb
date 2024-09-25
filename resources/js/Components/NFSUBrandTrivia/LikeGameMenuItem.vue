ef2489a755e82086a312605732bb22df96b70839
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-09-25 09:43:57 +0300

    wip (LikeGameMenuItem)

--- resources/js/Components/NFSUBrandTrivia/LikeGameMenuItem.vue
+++ resources/js/Components/NFSUBrandTrivia/LikeGameMenuItem.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { computed, ref } from "vue";
 import { Link } from "@inertiajs/vue3";
 
 defineProps({
@@ -8,47 +7,43 @@ defineProps({
     required: true,
   },
 });
-
-const hovered = ref(false);
-
-const containerClasses = computed(() =>
-  hovered.value
-    ? "border-b-[12px] border-t-[12px] border-gray-300"
-    : "py-[12px]",
-);
 </script>
 
 <template>
-  <div class="relative h-16 text-nowrap" :class="containerClasses">
-    <div v-if="hovered">
-      <div
-        class="absolute -left-[6px] -top-[12px] h-[12px] w-[12px] skew-x-[45deg] bg-gray-300"
-      ></div>
-      <div
-        class="absolute -left-[6px] -bottom-[12px] h-[12px] w-[12px] -skew-x-[45deg] bg-gray-300"
-      ></div>
-      <div
-        class="animate-blinking absolute -right-[21px] -top-[3px] -my-2.5 h-[66px] w-5 bg-gray-500"
-      ></div>
-    </div>
-
+  <li class="relative h-16 text-nowrap">
     <Link
       :href="href"
-      @focusin="hovered = true"
-      @focusout="hovered = false"
-      @mouseover="hovered = true"
-      @mouseleave="hovered = false"
-      class="flex w-full justify-end pr-2 font-sans font-semibold text-3xl uppercase italic tracking-wide text-sky-400 hover:text-gray-300 focus:text-gray-300 focus:outline-none"
+      class="flex w-full justify-end pr-2 font-sans font-semibold text-3xl uppercase italic tracking-wide text-sky-400 hover:text-gray-300 focus:text-gray-300 focus:outline-none border-transparent border-l-[12px] border-b-[12px] border-t-[12px]"
     >
-      <slot></slot>
+      <slot />
     </Link>
-  </div>
+  </li>
 </template>
 
 <style scoped>
-.animate-blinking {
-  -webkit-animation: animate-blinking 1s infinite;
-  animation: animate-blinking 1s infinite;
+li {
+  list-style: none;
+}
+
+li:hover a {
+  border-top: 12px solid #2b3c46;
+  border-bottom: 12px solid #2b3c46;
+  color: #cde7f1;
+}
+
+li:hover::after {
+  display: block;
+  width: 19px;
+  height: 62px;
+  position: absolute;
+  top: 0;
+  right: -19px;
+  content: "";
+  background-color: rgb(107 114 128);
+  -webkit-animation: animate-blinking 1s infinite; /* Safari 4+ */
+  -moz-animation: animate-blinking 1s infinite; /* Fx 5+ */
+  -o-animation: animate-blinking 1s infinite; /* Opera 12+ */
+  animation: animate-blinking 1s infinite; /* IE 10+, Fx 29+ */
 }
 
 @-webkit-keyframes animate-blinking {
